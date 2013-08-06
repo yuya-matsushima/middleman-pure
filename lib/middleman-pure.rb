3ef5fb5c55b002f536103e9f33c2dfffcbcb59f7
yterajima <terra@e2esound.com>
2013-08-07 01:35:58 +0900

    first commit

---
+++ lib/middleman-pure.rb
@@ -0,0 +1,7 @@
+require "middleman-pure/version"
+
+module Middleman
+  module Pure
+    # Your code goes here...
+  end
+end
---
+++ lib/middleman-pure/version.rb
@@ -0,0 +1,5 @@
+module Middleman
+  module Pure
+    VERSION = "0.0.1"
+  end
+end
